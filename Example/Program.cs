03f252c72bc91c45cf772cb3626995a49a2fdf09
Daniel Brazil <daniel_brazil@technologyonecorp.com>
2018-09-13 09:45:08 +1000

    update

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,86 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Emit;
-
-namespace ConsoleApplication
-{
-    public class Program
+﻿    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Loader;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Emit;
+    
+    namespace ConsoleApplication
     {
-        static Action<string> Write = Console.WriteLine;
-        
-        public static void Main(string[] args)
+        public class Program
         {
-            Write("Let's compile!");
+            static Action<string> Write = Console.WriteLine;
             
-            string codeToCompile = @"
-            using System;
-
-            namespace RoslynCompileSample
+            public static void Main(string[] args)
             {
-                public class Writer
+                Write("Let's compile!");
+                
+                string codeToCompile = @"
+                using System;
+                using System.Runtime;
+                namespace RoslynCompileSample
                 {
-                    public void Write(string message)
+                    public class Writer
                     {
-                        Console.WriteLine($""you said '{message}!'"");
+                        public void Write(string message)
+                        {
+                            Console.WriteLine($""you said '{message}!'"");
+                        }
+                    }
+                }";
+                
+                Write("Parsing the code into the SyntaxTree");
+                
+                SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
+
+
+                string assemblyName = Path.GetRandomFileName();
+
+                MetadataReference[] references = new MetadataReference[]
+                    {
+                        MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location),
+                       MetadataReference.CreateFromFile(typeof(Console).GetTypeInfo().Assembly.Location),
+                        MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\2.1.2\System.Runtime.dll")
+                  
+               
+                    };
+        
+                Write("Compiling ...");
+                CSharpCompilation compilation = CSharpCompilation.Create(
+                    assemblyName,
+                    syntaxTrees: new[] { syntaxTree },
+                    references: references,
+                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+        
+                using (var ms = new MemoryStream())
+                {
+                    EmitResult result = compilation.Emit(ms);
+    
+                    if (!result.Success)
+                    {
+                        Write("Compilation failed!");
+                        IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
+                            diagnostic.IsWarningAsError || 
+                            diagnostic.Severity == DiagnosticSeverity.Error);
+    
+                        foreach (Diagnostic diagnostic in failures)
+                        {
+                            Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                        }
+                    }
+                    else
+                    {
+                        Write("Compilation successful! Now instantiating and executing the code ...");
+                        ms.Seek(0, SeekOrigin.Begin);
+                        
+                        Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                        var type= assembly.GetType("RoslynCompileSample.Writer");
+                        var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
+                        var meth = type.GetMember("Write").First() as MethodInfo;
+                        meth.Invoke(instance, new [] {"joel"});
                     }
                 }
-            }";
-            
-            Write("Parsing the code into the SyntaxTree");
-            
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
-    
-            string assemblyName = Path.GetRandomFileName();
-  
-            MetadataReference[] references = new MetadataReference[]
-            {
-                MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location)
-       
-            };
-
-            Write("Compiling ...");
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                assemblyName,
-                syntaxTrees: new[] { syntaxTree },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-    
-            using (var ms = new MemoryStream())
-            {
-                EmitResult result = compilation.Emit(ms);
-
-                if (!result.Success)
-                {
-                    Write("Compilation failed!");
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
-                        diagnostic.IsWarningAsError || 
-                        diagnostic.Severity == DiagnosticSeverity.Error);
-
-                    foreach (Diagnostic diagnostic in failures)
-                    {
-                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
-                    }
-                }
-                else
-                {
-                    Write("Compilation successful! Now instantiating and executing the code ...");
-                    ms.Seek(0, SeekOrigin.Begin);
-                    
-                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
-                    var type= assembly.GetType("RoslynCompileSample.Writer");
-                    var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
-                    var meth = type.GetMember("Write").First() as MethodInfo;
-                    meth.Invoke(instance, new [] {"joel"});
-                }
+        
+        
             }
-    
-    
         }
     }
-}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# RoslynCompile
+This example, dynamically compiles code in .net core 2.
+
+I have directly linked to: System.Runtime.dll, in the code on line 46, this might need to change for you.
